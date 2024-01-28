cb8a99dd62d2b69c7f1b5aead3496b97ce1632cd
Vishnu200399 <hackingup3@gmail.com>
2024-01-28 16:56:58 +0530

    FirstCommit

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/ncF4PjV3/Colorful-Pastel-Creative-Project-Presentation.png',
+    title: "Fitness To Fire",
+    desc: "This is an Front-end web applications that includes an inbuilt BMI calculator.This shows my html,css,and javascript knowledge combined",
+    link: "https://vishnu200399.github.io/Fitness_To_Fire/",
+    git: "https://github.com/Vishnu200399/Fitness_To_Fire",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/mk2vdYdQ/Beige-Vintage-Group-Project-Presentation.png',
+    title: "Multi-Disease Web predictor",
+    desc: "A multi-disease prediction system that is bases on machine learnig with the training of labelled data",
+    link:"https://mlwebapp-ykqdqefdbpkxryzgub2gnl.streamlit.app/",
+    git: "https://github.com/Vishnu200399/medical_web_app",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/N0t2KFJ3/Colorful-Watercolor-Creative-Project-Presentation.png',
+    title: "MovieLand",
+    desc: "This is an React app that lets you to find movies of same name or a movie recommendation system with OMDP api",
+    link: "https://vishnu200399.github.io/Movie_Recommendation_React/",
+    git: "https://github.com/Vishnu200399/Movie_Recommendation_React",
+  },
+  {
+    id: 4,
+    img: 'https://i.postimg.cc/85HDLjj9/Blue-Doodle-Project-Presentation.png',
+    title: "Landing Page with GSAP",
+    desc: "This is an simple Landing page where i used GSAP to add eye-catching animations on the screen",
+    link: "https://vishnu200399.github.io/Landing_page1/",
+    git: "https://github.com/Vishnu200399/Landing_page1/tree/main",
+  },
+  {
+    id: 5,
+    img: 'https://i.postimg.cc/FRLfgbjz/Black-Doodle-Group-Project-Presentation.png',
+    title: "Scroll Trigger with GSAP",
+    desc: "This is an simple Landing page where i used GSAP to add Scroll-Trigger animations",
+    link: "https://vishnu200399.github.io/Landing_page2/",
+    git: "https://github.com/Vishnu200399/Landing_page2/tree/main",
+  },
+  
+];
